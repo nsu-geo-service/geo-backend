e1f4843c13feacf99471472c92ec7e9ddd64a97e
JKearnsl <boyarshinovn@gmail.com>
2024-01-30 08:25:02 +0300

    Исправление ошибок

--- src/geo/views/__init__.py
+++ src/geo/views/__init__.py
@@ -1,9 +1,4 @@
 from .base import BaseView
 
-from .user import UserResponse
-from .user import UserSmallResponse
-
-from .role import RoleResponse
-from .role import RolesResponse
-from .role import PermissionResponse
-from .role import PermissionsResponse
+from .task import TaskResponse
+from .task import TasksResponse
--- src/geo/views/base.py
+++ src/geo/views/base.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from pydantic import BaseModel
 
-from audiolibrary.models.schemas import Error
+from geo.models.schemas import Error
 
 
 class BaseView(BaseModel):
