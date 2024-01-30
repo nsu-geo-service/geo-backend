f265dba012eda1c8182f7f18b266c999106d02f8
JKearnsl <boyarshinovn@gmail.com>
2024-01-30 06:35:13 +0300

    Реформат конфигурационного файла

--- src/geo/config.py
+++ src/geo/config.py
@@ -6,13 +6,8 @@ from dotenv import load_dotenv
 logger = getLogger(__name__)
 
 DEBUG_ENV = "DEBUG"
-POSTGRES_HOST_ENV = "POSTGRES_HOST"
-POSTGRES_PORT_ENV = "POSTGRES_PORT"
-POSTGRES_USERNAME_ENV = "POSTGRES_USERNAME"
-POSTGRES_PASSWORD_ENV = "POSTGRES_PASSWORD"
-POSTGRES_DATABASE_ENV = "POSTGRES_DATABASE"
-
-JWT_SECRET_KEY_ENV = "JWT_SECRET_KEY"
+REDIS_HOST_ENV = "REDIS_HOST"
+REDIS_PORT_ENV = "REDIS_PORT"
 
 
 class ConfigParseError(ValueError):
@@ -20,40 +15,15 @@ class ConfigParseError(ValueError):
 
 
 @dataclass
-class PostgresConfig:
-    DATABASE: str
-    USERNAME: str
-    PASSWORD: str
+class RedisConfig:
     HOST: str
     PORT: int
 
 
 @dataclass
-class DbConfig:
-    POSTGRESQL: PostgresConfig
-
-
-@dataclass
-class JWTConfig:
-    SECRET_KEY: str
-
-
-@dataclass
-class S3Config:
-    BUCKET: str
-    ENDPOINT_URL: str
-    PUBLIC_ENDPOINT_URL: str
-    REGION: str
-    ACCESS_KEY_ID: str
-    ACCESS_KEY: str
-
-
-@dataclass
 class Config:
     DEBUG: bool
-    DB: DbConfig
-    JWT: JWTConfig
-    S3: S3Config
+    REDIS: RedisConfig
 
 
 def to_bool(value) -> bool:
@@ -66,6 +36,14 @@ def get_str_env(key: str, optional: bool = False) -> str:
         logger.error("%s is not set", key)
         raise ConfigParseError(f"{key} is not set")
     return val
+
+
+def get_int_env(key: str, optional: bool = False) -> int:
+    val = get_str_env(key, optional)
+    try:
+        return int(val)
+    except ValueError:
+        raise ConfigParseError("Value %s must be integer", val)
 
 
 def load_env_config(env_file: str | os.PathLike = None) -> Config:
@@ -81,24 +59,8 @@ def load_env_config(env_file: str | os.PathLike = None) -> Config:
 
     return Config(
         DEBUG=to_bool(get_str_env(DEBUG_ENV)),
-        DB=DbConfig(
-            POSTGRESQL=PostgresConfig(
-                HOST=get_str_env(POSTGRES_HOST_ENV),
-                PORT=int(get_str_env(POSTGRES_PORT_ENV)),
-                USERNAME=get_str_env(POSTGRES_USERNAME_ENV),
-                PASSWORD=get_str_env(POSTGRES_PASSWORD_ENV),
-                DATABASE=get_str_env(POSTGRES_DATABASE_ENV),
-            )
-        ),
-        JWT=JWTConfig(
-            SECRET_KEY=get_str_env(JWT_SECRET_KEY_ENV),
-        ),
-        S3=S3Config(
-            BUCKET=get_str_env("S3_BUCKET"),
-            ENDPOINT_URL=get_str_env("S3_ENDPOINT_URL"),
-            PUBLIC_ENDPOINT_URL=get_str_env("S3_PUBLIC_ENDPOINT_URL", optional=True),
-            REGION=get_str_env("S3_REGION"),
-            ACCESS_KEY_ID=get_str_env("S3_ACCESS_KEY_ID"),
-            ACCESS_KEY=get_str_env("S3_ACCESS_KEY"),
+        REDIS=RedisConfig(
+            HOST=get_str_env(REDIS_HOST_ENV),
+            PORT=get_int_env(REDIS_PORT_ENV)
         )
     )
