45d86fb198c2ef14ea1e44149ba4fb5f11250c20
JKearnsl <boyarshinovn@gmail.com>
2024-01-30 08:26:49 +0300

    Добавлена инициализация redis в lifespan

--- src/geo/lifespan.py
+++ src/geo/lifespan.py
@@ -1,9 +1,20 @@
 import logging
+
+from redis.asyncio import Redis
 from fastapi import FastAPI
 
-from geo.utils.http import AiohttpClient
+from geo.config import Config, RedisConfig
+from geo.utils.redis import RedisClient
 
-from geo.config import Config
+
+async def init_redis_pool(app: FastAPI, config: RedisConfig):
+    pool_0 = await Redis.from_url(
+        f"redis://{config.HOST}:{config.PORT}/0",
+        encoding="utf-8",
+        decode_responses=True,
+    )
+    getattr(app, "state").redis_queue = RedisClient(pool_0)
+
 
 class LifeSpan:
 
@@ -13,7 +24,9 @@ class LifeSpan:
 
     async def startup_handler(self) -> None:
         logging.debug("Выполнение FastAPI startup event handler.")
+        await init_redis_pool(self._app, self._config.REDIS)
         logging.info("FastAPI Успешно запущен.")
 
     async def shutdown_handler(self) -> None:
         logging.debug("Выполнение FastAPI shutdown event handler.")
+        await getattr(self._app, "state").redis_queue.close()
