1eda86f05ad41434f3b6f5a27aca7869e4ea26d7
JKearnsl <boyarshinovn@gmail.com>
2024-01-30 08:25:23 +0300

    Добавлены модели представления

---
+++ src/geo/views/task.py
@@ -0,0 +1,10 @@
+from .base import BaseView
+from geo.models import schemas
+
+
+class TaskResponse(BaseView):
+    content: schemas.Task
+
+
+class TasksResponse(BaseView):
+    content: list[schemas.Task]
