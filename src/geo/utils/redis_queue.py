e6485f308d6c2619b69aa102e5cd17406e4e38cd
JKearnsl <boyarshinovn@gmail.com>
2024-01-30 18:48:55 +0300

    Исправлены ошибки; Теперь RedisQueue полностью асинхронный

--- src/geo/utils/redis_queue.py
+++ src/geo/utils/redis_queue.py
@@ -1,14 +1,23 @@
+from typing import Any
+
+from redis.asyncio import Redis
+
+from geo.utils.redis import RedisClient
+
 
 class RedisQueue:
-    def __init__(self, client, namespace: str = 'queue'):
+    def __init__(self, client: Redis | RedisClient | Any, namespace: str = 'queue'):
         self._client = client
         self.key = namespace
 
-    def enqueue(self, item):
-        self._client.rpush(self.key, item)
+    async def enqueue(self, *values: bytes | memoryview | str | int | float) -> None:
+        await self._client.rpush(self.key, *values)
 
-    def dequeue(self):
-        return self._client.lpop(self.key)
+    async def dequeue(self) -> str | list | None:
+        return await self._client.lpop(self.key)
 
-    def is_empty(self):
-        return self._client.llen(self.key) == 0
+    async def is_empty(self) -> bool:
+        return await self._client.llen(self.key) == 0
+
+    async def close(self) -> None:
+        await self._client.close()
