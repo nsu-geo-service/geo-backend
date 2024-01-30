0f00073df62a59e661eac2f6da6a7414d88ef6f4
JKearnsl <boyarshinovn@gmail.com>
2024-01-30 18:47:35 +0300

    Обновлены зависимости

--- src/geo/services/__init__.py
+++ src/geo/services/__init__.py
@@ -13,16 +13,19 @@ class ServiceFactory:
             redis_client: RedisClient,
             data_queue: RedisQueue,
             tomography_queue: RedisQueue,
+            redis_query_base: RedisClient,
     ):
         self._redis_client = redis_client
         self._data_queue = data_queue
         self._tomography_queue = tomography_queue
+        self._redis_query_base = redis_query_base
         self._config = config
 
     @property
     def task(self) -> TaskApplicationService:
         return TaskApplicationService(
             redis_client=self._redis_client,
+            redis_query_base=self._redis_query_base,
         )
 
     @property
@@ -31,6 +34,7 @@ class ServiceFactory:
             redis_client=self._redis_client,
             data_queue=self._data_queue,
             tomography_queue=self._tomography_queue,
+            redis_query_base=self._redis_query_base,
         )
 
     @property
