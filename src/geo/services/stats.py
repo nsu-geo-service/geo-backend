4f13fcee89ebf7cb1706154d43283b3d8a925d6e
JKearnsl <boyarshinovn@gmail.com>
2024-01-30 13:42:39 +0300

    Обновлены зависимости

--- src/geo/services/stats.py
+++ src/geo/services/stats.py
@@ -1,12 +1,12 @@
 
 class StatsApplicationService:
 
-    def __init__(self, redis_queue, config):
-        self._redis_queue = redis_queue
+    def __init__(self, redis_client, config):
+        self._redis_client = redis_client
         self._config = config
 
     async def get_stats(self) -> dict:
         return {
             "version": self._config.BASE.VERSION,
-            "redis": await self._redis_queue.ping()
+            "redis": await self._redis_client.ping()
         }
--- src/geo/services/task.py
+++ src/geo/services/task.py
@@ -11,9 +11,9 @@ class TaskApplicationService:
 
     def __init__(
             self,
-            redis_queue: RedisClient,
+            redis_client: RedisClient,
     ):
-        self._redis_queue = redis_queue
+        self._redis_client = redis_client
 
     async def list(self, page: int, per_page: int) -> list[Task]:
         per_page_limit = 40
@@ -21,12 +21,12 @@ class TaskApplicationService:
         per_page = min(per_page, per_page_limit, 2147483646)
         offset = min((page - 1) * per_page, 2147483646)
 
-        sorted_tasks = await self._redis_queue.zrange('queue', 0, -1, with_scores=True)
+        sorted_tasks = await self._redis_client.zrange('tasks', 0, -1, with_scores=True)
         trimmed_task_ids = [item[0] for item in sorted_tasks[offset:offset + per_page]]
 
         raw_tasks = await asyncio.gather(
             *[
-                self._redis_queue.hgetall(hash_key=task_id)
+                self._redis_client.hgetall(hash_key=task_id)
                 for task_id in trimmed_task_ids
             ]
         )
@@ -35,7 +35,7 @@ class TaskApplicationService:
             Task(
                 id=TaskID(uuid.UUID(task_id)),
                 state=TaskState(raw_task['state']),
-                step=TaskStep(raw_task['step']),
+                step=TaskStep(raw_task['step']) if raw_task['step'] else None,
                 created_at=datetime.datetime.fromtimestamp(
                     float(raw_task['created_at']),
                     tz=datetime.UTC
@@ -49,13 +49,13 @@ class TaskApplicationService:
         ]
 
     async def get_task(self, task_id: TaskID) -> Task:
-        raw_task = await self._redis_queue.hgetall(hash_key=str(task_id))
+        raw_task = await self._redis_client.hgetall(hash_key=str(task_id))
         if not raw_task:
             raise NotFound(f"Задача с id {task_id!r} не найдена")
         return Task(
             id=task_id,
             state=TaskState(raw_task['state']),
-            step=TaskStep(raw_task['step']),
+            step=TaskStep(raw_task['step']) if raw_task['step'] else None,
             created_at=datetime.datetime.fromtimestamp(
                 float(raw_task['created_at']),
                 tz=datetime.UTC
@@ -70,29 +70,29 @@ class TaskApplicationService:
         task = Task(
             id=TaskID(uuid.uuid4()),
             state=TaskState.PLAIN,
-            step=TaskStep.STEP1,
+            step=None,
             created_at=datetime.datetime.now(tz=datetime.UTC),
             completed_in=None,
         )
-        await self._redis_queue.hset(
+        await self._redis_client.hset(
             hash_key=str(task.id),
             mapping={
                 'state': task.state.value,
-                'step': task.step.value,
+                'step': task.step.value if task.step else '',
                 'created_at': task.created_at.timestamp(),
-                'completed_in': task.completed_in.timestamp() if task.completed_in else "",
+                'completed_in': task.completed_in.timestamp() if task.completed_in else '',
             }
         )
-        await self._redis_queue.zadd('queue', {str(task.id): task.created_at.timestamp()})
+        await self._redis_client.zadd('tasks', {str(task.id): task.created_at.timestamp()})
         return task
 
     async def delete_task(self, task_id: TaskID) -> None:
-        key_exists = await self._redis_queue.exists(str(task_id))
+        key_exists = await self._redis_client.exists(str(task_id))
         if not key_exists:
             raise NotFound(f"Задача с id {task_id!r} не найдена")
 
-        await self._redis_queue.zrem('queue', str(task_id))
-        await self._redis_queue.delete(str(task_id))
+        await self._redis_client.zrem('tasks', str(task_id))
+        await self._redis_client.delete(str(task_id))
 
     async def count(self) -> int:
-        return await self._redis_queue.zcard('queue')
+        return await self._redis_client.zcard('tasks')
