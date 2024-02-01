ebedd1070d2627d13244bc7d3593bef571461d9d
JKearnsl <boyarshinovn@gmail.com>
2024-02-01 23:06:13 +0300

    Обновлен нейминг

--- src/geo/controllers/proc.py
+++ src/geo/controllers/proc.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends
 from fastapi import status as http_status
 
 from geo.models.schemas import TaskID
-from geo.models.schemas.data import DataProc
-from geo.models.schemas.tomography import TomographyProc
+from geo.models.schemas.data import SeisData
+from geo.models.schemas.tomography import Tomography
 from geo.services import ServiceFactory
 from geo.services.di import get_services
 
@@ -13,20 +13,20 @@ proc_router = APIRouter(prefix="/proc", tags=["Process"])
 @proc_router.post("/data/{task_id}", response_model=None, status_code=http_status.HTTP_202_ACCEPTED)
 async def data_proc(
         task_id: TaskID,
-        data: DataProc,
+        data: SeisData,
         services: ServiceFactory = Depends(get_services)
 ):
     """
     Запуск процесса обработки данных
 
     """
-    await services.geo.data_proc(task_id=task_id, data=data)
+    await services.geo.seisdata_proc(task_id=task_id, data=data)
 
 
 @proc_router.post("/tomography/{task_id}", response_model=None, status_code=http_status.HTTP_202_ACCEPTED)
 async def tomography_proc(
         task_id: TaskID,
-        data: TomographyProc,
+        data: Tomography,
         services: ServiceFactory = Depends(get_services)
 ):
     """
