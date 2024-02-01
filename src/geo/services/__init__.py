9df0de8c44c6cd560b465109e381c5ace0dd1a84
JKearnsl <boyarshinovn@gmail.com>
2024-02-01 23:07:30 +0300

    Удален Redis

--- src/geo/services/__init__.py
+++ src/geo/services/__init__.py
@@ -1,42 +1,39 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+
 from geo.config import Config
 from geo.services.geo import GeoApplicationService
 from geo.services.stats import StatsApplicationService
 from geo.services.task import TaskApplicationService
-from geo.utils.redis import RedisClient
-from geo.utils.redis_queue import RedisQueue
+from geo.utils.queue import Queue
 
 
 class ServiceFactory:
     def __init__(
             self,
             config: Config,
-            redis_client: RedisClient,
-            data_queue: RedisQueue,
-            tomography_queue: RedisQueue,
-            redis_query_base: RedisClient,
+            lazy_session: async_sessionmaker[AsyncSession],
+            data_queue: Queue,
+            tomography_queue: Queue,
     ):
-        self._redis_client = redis_client
+        self._lazy_session = lazy_session
         self._data_queue = data_queue
         self._tomography_queue = tomography_queue
-        self._redis_query_base = redis_query_base
         self._config = config
 
     @property
     def task(self) -> TaskApplicationService:
         return TaskApplicationService(
-            redis_client=self._redis_client,
-            redis_query_base=self._redis_query_base,
+            lazy_session=self._lazy_session,
         )
 
     @property
     def geo(self) -> GeoApplicationService:
         return GeoApplicationService(
-            redis_client=self._redis_client,
             data_queue=self._data_queue,
             tomography_queue=self._tomography_queue,
-            redis_query_base=self._redis_query_base,
+            lazy_session=self._lazy_session,
         )
 
     @property
     def stats(self) -> StatsApplicationService:
-        return StatsApplicationService(redis_client=self._redis_client, config=self._config)
+        return StatsApplicationService(config=self._config)
