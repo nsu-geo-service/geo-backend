fe0f9ec78cd4b757fae52034b12e96bd53bb6983
JKearnsl <boyarshinovn@gmail.com>
2024-02-02 13:50:03 +0300

    Добавлен get_all_by_task

--- src/geo/repositories/detection.py
+++ src/geo/repositories/detection.py
@@ -1,6 +1,43 @@
+from uuid import UUID
+
+from sqlalchemy import text, select
+from sqlalchemy.orm import subqueryload
+
 from geo.models import tables
 from geo.repositories.base import BaseRepository
 
 
 class DetectionRepo(BaseRepository[tables.Detection]):
     table = tables.Detection
+
+    async def get_all_by_task(
+            self,
+            task_id: UUID,
+            limit: int = None,
+            offset: int = None,
+            order_by: str = "id",
+            **kwargs
+    ) -> list[tables.Detection]:
+        """
+        Получает все записи
+
+        :param task_id: идентификатор задачи
+        :param limit: лимит 100
+        :param offset: смещение 0
+        :param kwargs: filter by
+        :param order_by: сортировка
+        :return:
+        """
+        stmt = (
+            select(self.table)
+            .options(subqueryload(self.table.event))
+            .where(self.table.event.has(task_id=task_id))
+            .order_by(text(order_by))
+        )
+        if limit:
+            stmt = stmt.limit(limit)
+        if offset:
+            stmt = stmt.offset(offset)
+
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
